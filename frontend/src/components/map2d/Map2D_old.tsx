5edd76e427e3658e8b2ad3a07644a17174171861
Teyo-mat <matheo.kesar@gmail.com>
2025-11-06 01:11:24 +0100

    preview 2D mape (nedorađeno)

---
+++ frontend/src/components/map2d/Map2D_old.tsx
@@ -0,0 +1,24 @@
+// import type MapSelectionDescriptor from "../../interfaces/MapSelectionDescriptor";
+// import "./Map2D.css";
+
+// interface Props {
+//   onInput: (selection: MapSelectionDescriptor) => void;
+// }
+
+// export default function Map2D({ onInput }: Props) {
+//   const temp: MapSelectionDescriptor = {
+//     latitude: 45.0,
+//     longitude: 15.0,
+//   };
+
+//   return (
+//     <div
+//       className="map-2d card"
+//       onClick={() => {
+//         onInput(temp);
+//       }}
+//     >
+//       <img src="/vectors/earth.svg" alt="" />
+//     </div>
+//   );
+// }
--- frontend/src/components/pages/MapPage.tsx
+++ frontend/src/components/pages/MapPage.tsx
@@ -4,26 +4,30 @@ import Footer from "../general/Footer";
 import Header from "../general/Header";
 import Map2D from "../map2d/Map2D";
 import ButtonSignOut from "../profile/ButtonProfile";
-import "./MapPage.css"
+import "./MapPage.css";
 
 export default function MapPage() {
-    // useEffect(() => {
-    //     const script = document.createElement("script");
-    //     script.src = "/public/scripts/mapper/engine.js";
-    //     script.defer = true;
-    //     script.type = "module";
-    //     document.body.appendChild(script);
-    // }, [])
+  // useEffect(() => {
+  //     const script = document.createElement("script");
+  //     script.src = "/public/scripts/mapper/engine.js";
+  //     script.defer = true;
+  //     script.type = "module";
+  //     document.body.appendChild(script);
+  // }, [])
 
-    return (
-        <>
-            <Header>
-                <ButtonSignOut></ButtonSignOut>
-            </Header>
-            <main className="map-page">
-                <Map2D onInput={(selection: MapSelectionDescriptor) => {console.log(selection);}}/>
-            </main>
-            <Footer/>
-        </>
-    );
+  return (
+    <>
+      <Header>
+        <ButtonSignOut></ButtonSignOut>
+      </Header>
+      <main className="map-page">
+        <Map2D
+          onInput={(selection: MapSelectionDescriptor) => {
+            console.log(selection);
+          }}
+        />
+      </main>
+      <Footer />
+    </>
+  );
 }
