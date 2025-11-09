b3c420309c691b537e167bafc62c64c47d028a9b
Vito Čuić <vito.cuic@fer.hr>
2025-11-09 18:25:53 +0100

    Vite config dodatak porta iz env varijable

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+// https://vitejs.dev/config/
+export default defineConfig(() => {
+  const port = Number(process.env.PORT) || 5173  // Default to 5173 if not set
+
+  return {
+    plugins: [react()],
+    server: {
+      port,
+      host: true, // makes it accessible from outside the container
+    },
+    preview: {
+      port,
+      host: true,
+    },
+  }
 })
