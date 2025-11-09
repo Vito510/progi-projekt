4fe83bc0bcd92b24b59ede8706f640f2fb1881e2
Vinko Grancic <vinko.grancic@tahoma.hr>
2025-11-09 09:40:56 +0100

    login/register

--- backend/src/main/java/hr/fer/progi/progi_projekt/controller/AuthController.java
+++ backend/src/main/java/hr/fer/progi/progi_projekt/controller/AuthController.java
@@ -1,18 +1,26 @@
-package hr.fer.progi.progi_projekt.controller;
+package hr.fer.progi.projekt.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
 
 @RestController
 public class AuthController {
-    @GetMapping("/login")
-    public void login(){
-        // TODO login
+
+    @GetMapping("/me")
+    public Map<String, Object> getCurrentUser(@AuthenticationPrincipal OAuth2User user) {
+        if (user == null) {
+            return Map.of("authenticated", false);
+        }
+
+        return Map.of(
+                "authenticated", true,
+                "name", user.getAttribute("name"),
+                "email", user.getAttribute("email"),
+        );
     }
 
-    @GetMapping("/logout")
-    public void logout(){
-        // TODO logout
-    }
 }
