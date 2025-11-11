ad6c9e7e1a968239002c3b930b8560d7908f0b3c
Martin Golub <martin.golub@fer.unizg.hr>
2025-11-11 09:44:11 +0100

    Vraćanje stila login stranice

--- frontend/src/components/pages/LoginPage.tsx
+++ frontend/src/components/pages/LoginPage.tsx
@@ -1,12 +1,27 @@
 import { useAuth } from "../../context/AuthContext";
+import Header from "../general/Header";
+import './LoginPage.css';
 
 export default function LoginPage() {
   const { login } = useAuth();
 
   return (
-    <div>
-      <h1>Login</h1>
-      <button onClick={login}>Login with Google</button>
-    </div>
+    <>
+      <Header></Header>
+      <main className="login-page">
+        <div className="login-card">
+
+          <div className="login-header">
+            <h1 className="login-title">Prijava</h1>
+            <p className="login-subtitle">Prijavite se u svoji profil</p>
+          </div>
+
+          <div className="login-body">
+            <button type="button" onClick={login} className="login-button">Login with Google</button>
+          </div>
+
+        </div>
+      </main>
+    </>
   );
 }
--- frontend/src/components/pages/LoginSuccesPage.tsx
+++ frontend/src/components/pages/LoginSuccesPage.tsx
@@ -1,14 +1,54 @@
+import type { ReactNode } from "react";
 import { useAuth } from "../../context/AuthContext";
+import Button from "../general/Button";
+import Header from "../general/Header";
+import ButtonProfile from "../profile/ButtonProfile";
+import ButtonSignOut from "../profile/ButtonSignOut";
+import './LoginSuccessPage.css';
+import { Link } from "react-router-dom";
 
 export default function LoginSuccessPage() {
   const { user, loading } = useAuth();
 
   if (loading) return <p>Loading...</p>;
 
+  let card: ReactNode;
+  if (loading) {
+    card = (
+      <div className="card">
+          <header>
+            <h1>Učitavanje...</h1>
+          </header>
+      </div>
+    );
+  } else {
+    card = (
+      <div className="card">
+        <header>
+          <h1>Pozdrav, {user?.name}!</h1>
+          <p>Uspješno ste se prijavili u aplikaciju.</p>
+        </header>
+
+        <section>
+          <Link to={"/"}>
+            <Button>
+              <p>Glavna stranica</p>
+              <i className="fa fa-home fa-lg"></i>
+            </Button>
+          </Link>
+          <ButtonProfile></ButtonProfile>
+          <ButtonSignOut></ButtonSignOut>
+        </section>
+      </div>
+    );
+  }
+
   return (
-    <div>
-      <h1>Pozdrav, {user?.name}!</h1>
-      <p>Uspješno ste se prijavili u aplikaciju.</p>
-    </div>
+    <>
+      <Header></Header>
+      <main className="login-success-page">
+          {card}
+      </main>
+    </>
   );
 }
