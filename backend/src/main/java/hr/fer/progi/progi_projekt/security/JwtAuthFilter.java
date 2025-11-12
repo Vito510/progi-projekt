c858dcb792298794f6704d70010d29506048a1ed
Vito Čuić <vito.cuic@fer.hr>
2025-11-12 19:09:57 +0100

    Implement JWT authentication with JwtAuthFilter and JwtUtil classes

--- backend/src/main/java/hr/fer/progi/progi_projekt/security/JwtAuthFilter.java
+++ backend/src/main/java/hr/fer/progi/progi_projekt/security/JwtAuthFilter.java
@@ -1,4 +1,66 @@
 package hr.fer.progi.progi_projekt.security;
 
-public class JwtAuthFilter {
+
+import hr.fer.progi.progi_projekt.util.JwtUtil;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+import java.util.Collections;
+
+@Component
+public class JwtAuthFilter extends OncePerRequestFilter {
+
+    private final JwtUtil jwtUtil;
+
+    public JwtAuthFilter(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String jwt = null;
+        String email = null;
+
+        if (request.getCookies() != null) {
+            for (Cookie cookie : request.getCookies()) {
+                if ("jwt".equals(cookie.getName())) {
+                    jwt = cookie.getValue();
+                    break;
+                }
+            }
+        }
+
+        if (jwt != null) {
+            email = jwtUtil.extractUsername(jwt);
+        }
+
+        if (email != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            if (jwtUtil.isTokenValid(jwt, email)) {
+                UsernamePasswordAuthenticationToken authToken =
+                        new UsernamePasswordAuthenticationToken(
+                                new User(email, "", Collections.emptyList()),
+                                null,
+                                Collections.emptyList()
+                        );
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
 }
--- backend/src/main/java/hr/fer/progi/progi_projekt/security/SecurityConfig.java
+++ backend/src/main/java/hr/fer/progi/progi_projekt/security/SecurityConfig.java
@@ -1,10 +1,13 @@
 package hr.fer.progi.progi_projekt.security;
 
+import hr.fer.progi.progi_projekt.util.JwtUtil;
+import jakarta.servlet.http.Cookie;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -13,7 +16,7 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public SecurityFilterChain securityFilterChain(HttpSecurity http, JwtAuthFilter jwtAuthFilter) throws Exception {
         http
                 .cors(Customizer.withDefaults())
                 .authorizeHttpRequests(auth -> auth
@@ -21,6 +24,12 @@ public class SecurityConfig {
                 )
                         .oauth2Login(oauth2 -> oauth2
                                 .successHandler((request, response, authentication) -> {
+
+                                    String userEmail = authentication.getName();
+                                    System.out.println(userEmail);
+                                    Cookie cookie = createCookie(userEmail);
+                                    response.addCookie(cookie);
+
                                     response.sendRedirect("https://planinarko.onrender.com/login-success");
                                 })
                         )                .logout(logout -> logout
@@ -29,9 +38,22 @@ public class SecurityConfig {
                         .invalidateHttpSession(true)
                         .clearAuthentication(true)
                         .deleteCookies("JSESSIONID")
-                );
+                ).addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);;
 
         return http.build();
+    }
+
+    private static Cookie createCookie(String userEmail) {
+        JwtUtil jwtUtil = new JwtUtil();
+        String token = jwtUtil.generateToken(userEmail);
+
+        Cookie cookie = new Cookie("jwt", token);
+//        cookie.setHttpOnly(true);
+//        cookie.setSecure(true);
+        cookie.setPath("/");
+        cookie.setMaxAge(60 * 60 * 10);
+        cookie.setAttribute("SameSite", "None");
+        return cookie;
     }
 
     @Bean
