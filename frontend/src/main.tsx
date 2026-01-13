e12d62a4e79a21ac433c24e309cf5eac851e4c40
Martin Golub <martin.golub@fer.unizg.hr>
2026-01-13 08:04:53 +0100

    Iskljucen strict mode

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,9 +4,11 @@ import App from "./App.tsx";
 import { AuthProvider } from "./context/AuthContext.tsx";
 
 createRoot(document.getElementById("root")!).render(
-	<StrictMode>
-	<AuthProvider>
-		<App />
-	</AuthProvider>
-	</StrictMode>
+	<>
+		{/* <StrictMode> */}
+		<AuthProvider>
+			<App />
+		</AuthProvider>
+		{/* </StrictMode> */}
+	</>
 );
