470b6e4a1e9a10ba5012c2ff7ad1e8ba020a55ec
Vito Čuić <vito.cuic@fer.hr>
2026-01-21 20:17:10 +0100

    Fix: ButtonProfile na pocetnoj ne vodi do profil usera

--- frontend/src/components/profile/ButtonProfile.tsx
+++ frontend/src/components/profile/ButtonProfile.tsx
@@ -3,9 +3,8 @@ import { useAuth } from "../../context/AuthContext";
 
 export default function ButtonProfile() {
     const auth = useAuth();
-
     return (
-        <Button link="/profile">
+        <Button link={`/profile/${auth.user?.name}`}>
             <i className="fa fa-user fa-lg"></i>
             <p>{auth.user?.authenticated ? auth.user.name : "Profil"}</p>
         </Button>
