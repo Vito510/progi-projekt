4549dc6d061ac8efa24e41e6ce36df3891efc62e
Martin Golub <martin.golub@fer.unizg.hr>
2026-01-22 11:59:43 +0100

    Dodani edge case za ButtonCopyTrack

--- frontend/src/components/track/ButtonCopyTrack.tsx
+++ frontend/src/components/track/ButtonCopyTrack.tsx
@@ -3,41 +3,65 @@ import Button from "../general/Button";
 import type Track from "../../interfaces/Track";
 
 interface Props {
-  track: Track;
+    track: Track;
 }
 
 export default function ButtonCopyTrack({ track }: Props) {
-  const [copied, setCopied] = useState(false);
-  const timeoutRef = useRef<number | null>(null);
+    const [processing, setProcessing] = useState(false);
+    const [failed, setFailed] = useState(false);
+    const timeoutRef = useRef<number | null>(null);
 
-  useEffect(() => {
-    return () => {
-      if (timeoutRef.current) window.clearTimeout(timeoutRef.current);
+    useEffect(() => {
+        return () => {
+            if (timeoutRef.current) 
+                window.clearTimeout(timeoutRef.current);
+        };
+    }, []);
+
+    const handleCopyLink = async () => {
+        const fullLink = `${window.location.origin}/track/${track.id}`; 
+
+        try {
+            if (track.id != -1) {
+                await navigator.clipboard.writeText(fullLink);
+                setProcessing(true);
+                if (timeoutRef.current) 
+                    window.clearTimeout(timeoutRef.current);
+                timeoutRef.current = window.setTimeout(() => {setProcessing(false); setFailed(false)}, 1200);
+            } else {
+                setProcessing(true);
+                setFailed(true);
+                if (timeoutRef.current) 
+                    window.clearTimeout(timeoutRef.current);
+                timeoutRef.current = window.setTimeout(() => {setProcessing(false); setFailed(false)}, 1200);
+            }
+        } catch (err) {
+            console.error("Ne mogu kopirati link:", err);
+        }
     };
-  }, []);
 
-  const handleCopyLink = async () => {
-    const fullLink = `${window.location.origin}/track/${track.id}`; 
-
-    try {
-      await navigator.clipboard.writeText(fullLink);
-
-      setCopied(true);
-      if (timeoutRef.current) window.clearTimeout(timeoutRef.current);
-      timeoutRef.current = window.setTimeout(() => setCopied(false), 1200);
-    } catch (err) {
-      console.error("Ne mogu kopirati link:", err);
-    }
-  };
-
-  return (
-    <Button
-      type="primary"
-      onClick={handleCopyLink}
-      disabled={copied}     
-    >
-      <i className={`fa ${copied ? "fa-check" : "fa-copy"}`} />
-      <span>{copied ? "Kopirano" : "Kopiraj link"}</span>
-    </Button>
-  );
+    return (
+        <>
+            {processing ? 
+                <>
+                    {failed ?
+                        <Button type="quaternary" onClick={handleCopyLink} disabled>
+                            <i className="fa fa-times"/>
+                            <span>Spremi stazu</span>
+                        </Button>
+                    :
+                        <Button type="secondary" onClick={handleCopyLink} disabled>
+                            <i className="fa fa-check" />
+                            <span>Kopirano</span>
+                        </Button>
+                    }
+                </>
+                :
+                <Button type="secondary" onClick={handleCopyLink}>
+                    <i className="fa fa-copy"/>
+                    <span>Kopiraj link</span>
+                </Button>
+            }
+        </>
+    );
 }
